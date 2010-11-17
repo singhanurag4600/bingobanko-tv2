8dd006cf48fb8f2070e6754aa949e8a0391c3f2f
michael.toft@gmail.com <michael.toft@gmail.com@f5d97277-94af-402e-5e14-138e71a65268>
2010-11-17 18:03:47 +0000

    Fixing the downloader

--- trunk/src/SystemConfiguration.java
+++ trunk/src/SystemConfiguration.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * @author MTO
@@ -7,10 +9,15 @@ public class SystemConfiguration {
    public static final String ROOT_DIRECTORY;
    public static final String DATA_DIRECTORY;
    public static final String TRAINING_DIRECTORY;
+   public static final String PATTERN;
 
    static {
       // Fetch the current directory, with a possible default option
       ROOT_DIRECTORY = System.getProperty("user.dir", "C:/bingobanko/");
+
+      File patternFile = new File(ROOT_DIRECTORY, "pattern.txt");
+      PATTERN = readPattern(patternFile);
+
       // Initialize data directory
       File boardDirectory = new File(ROOT_DIRECTORY, "data");
       boardDirectory.mkdirs();
@@ -21,5 +28,20 @@ public class SystemConfiguration {
          throw new RuntimeException("Training directory must exist!");
       }
       TRAINING_DIRECTORY = trainingDirectory.getPath();
+   }
+
+   private static String readPattern(File patternFile) {
+      StringBuffer buf = new StringBuffer();
+      try {
+         int ch;
+         FileInputStream inputStream = new FileInputStream(patternFile);
+         while( (ch = inputStream.read()) != -1) {
+           buf.append((char)ch);
+         }
+         inputStream.close();
+      } catch (IOException e) {
+         throw new RuntimeException(e);
+      }
+      return buf.toString();
    }
 }
