a12250e08167473813f6812da38a665d13638a37
michael.toft@gmail.com <michael.toft@gmail.com@f5d97277-94af-402e-5e14-138e71a65268>
2010-11-24 14:41:40 +0000

    User agent fix

--- trunk/src/BoardDownloader.java
+++ trunk/src/BoardDownloader.java
@@ -20,34 +20,48 @@ import java.util.*;
  */
 public class BoardDownloader {
    private static GameOCRBusiness ocrScanner = new GameOCRBusiness();
+   
+   private static String[] userAgents = new String[] {
+      "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Trident/5.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR",
+      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; es-ES; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12",
+      "Mozilla/5.0 (Windows; U; Windows NT 5.1; fr; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12 (.NET CLR 3.5.30729)",
+      "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.12 (KHTML, like Gecko) Chrome/9.0.587.0 Safari/534.12",
+      "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)",
+      "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.12 (KHTML, like Gecko) Chrome/9.0.587.0 Safari/534.12",
+      "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5",
+      "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Trident/5.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR",
+      "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; GTB6.6; InfoPath.1; .NET CLR 2.0.50727; .NET CLR 3.0.450",
+      "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+      "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5) Gecko/20060127 Netscape/8.1",
+      "Mozilla/4.0 (compatible; MSIE 6.0; MSIE 5.5; Windows NT 5.1) Opera 7.02 [en]",
+      "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)",
+      "Mozilla/5.0 (X11; U; Linux i686 (x86_64); en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/3.0.198.0 Safari/532.0",
+      "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_7; en-US) AppleWebKit/531.0 (KHTML, like Gecko) Chrome/3.0.183 Safari/531.0"
+   };
 
    private static int boards = 0;
    private static int doubles = 0;
    private static final int FILENAME_SIZE = 51;
-   private static final String searchConstant = "/plader/";
    private static final String BINGOBANKO_URL = "bingobanko.tv2.dk";
+
+   private static String currentUserAgent = null;
 
    public static void main(String[] args) throws Exception {
       Random random = new Random();
 
-      String message = loadFromUrl("http://46.51.174.209/message3.txt");
-      if(message!=null && message.length()!=0) {
-         JOptionPane.showMessageDialog(new JFrame(), message);
-         System.exit(0);
-         return;
-      }
-
       BoardDownloader stripper = new BoardDownloader();
-      while(true) {
+      while (true) {
          try {
+            currentUserAgent = pickUserAgent();
             stripper.http();
-            long millis = (long)(random.nextDouble() * 35000) + 5000;
+            long millis = (long) (random.nextDouble() * 25000) + 5000;
             System.out.println("Venter " + millis + " milliseconds, saa tv2 ikke bliver sure...");
             Thread.sleep(millis);
          } catch (Exception e) {
             System.out.println("Ignoring error from server : " + e.getMessage());
+            e.printStackTrace();
             Thread.sleep(60000);
-         }                                                                                                   
+         }
       }
    }
 
@@ -68,28 +82,19 @@ public class BoardDownloader {
 
       NodeList list = parser.extractAllNodesThatMatch(filter);
       SimpleNodeIterator simpleNodeIterator = list.elements();
-      while(simpleNodeIterator.hasMoreNodes()) {
+      while (simpleNodeIterator.hasMoreNodes()) {
          Node node = simpleNodeIterator.nextNode();
-         if(node instanceof ImageTag) {
-            ImageTag img = (ImageTag)node;
+         if (node instanceof ImageTag) {
+            ImageTag img = (ImageTag) node;
             String attribute = img.getAttribute("src");
             String decoded = EncoderUtil.decode(attribute);
 
-            if(decoded!=null) {
+            if (decoded != null) {
                int position = decoded.indexOf("/" + String.valueOf(bingoIdx));
-               if(position!=-1) {
-                  String prefix = getPreviousBlock(decoded, position-1);
-                  String boardName = decoded.substring(position+1);
+               if (position != -1) {
+                  String prefix = getPreviousBlock(decoded, position - 1);
+                  String boardName = decoded.substring(position + 1);
                   fetchBoard(rootDir, prefix, boardName);
-               }  
-            }
-         } else if(node instanceof ScriptTag) {
-            ScriptTag tag = (ScriptTag)node;
-            String scriptCode = tag.getScriptCode();
-            if(scriptCode.indexOf(searchConstant)!=-1) {
-               ArrayList<String> boardNames = getBoards(scriptCode);
-               for (String boardName : boardNames) {
-                  fetchBoard(rootDir, searchConstant, boardName);
                }
             }
          }
@@ -98,71 +103,56 @@ public class BoardDownloader {
 
    private String getPreviousBlock(String decoded, int position) {
       StringBuffer previous = new StringBuffer();
-      for(int i=position;i>=0;i--) {
+      for (int i = position; i >= 0; i--) {
          char ch = decoded.charAt(i);
          previous.append(ch);
-         if(ch=='/') {
+         if (ch == '/') {
             break;
          }
       }
       return previous.reverse().toString();
    }
 
-   private ArrayList<String> getBoards(String s) throws Exception {
-      int nextStart = 0;
-      ArrayList<String> boards = new ArrayList<String>();
 
-      while(true) {
-         int startPosition = s.indexOf(searchConstant, nextStart);
-         if(startPosition==-1) {
-            break;
-         }
-         int endPosition = s.indexOf("\"", startPosition+searchConstant.length());
-
-         int realStart = startPosition + searchConstant.length();
-         String picName = s.substring(realStart, endPosition);
-
-         boards.add(picName);
-         nextStart = endPosition;
-      }
-      return boards;
-   }
-
-   private void fetchBoard(File rootDir, String prefix, String picName) throws IOException {
+   private static void fetchBoard(File rootDir, String prefix, String picName) throws IOException {
       BufferedImage image = readImage(prefix, picName);
-      if(image==null) {
+      if (image == null) {
          return;
       }
-      String kontrol = picName.substring(2,7);
+      String kontrol = picName.substring(2, 7);
 
-      if(image!=null) {
-         Plade plade = ocrScanner.recognize(image);
-         plade.setKontrolKode(kontrol);
-         String fileTitle = plade.getFileTitle();
-
-         File targetDir = new File(rootDir, fileTitle);
-         if(!targetDir.exists()) {
-            targetDir.mkdirs();
-
-            File targetFile = new File(targetDir, fileTitle);
-            ImageIO.write(image, "PNG", targetFile);
-
-            writeTextData(plade, targetDir);
-
-            writeBinaryData(plade, targetDir);
-            boards++;
-         } else {
-            doubles++;
-         }
+      if (image != null) {
+         processImage(rootDir, image, kontrol);
       }
-      System.out.println("Hentet plade " + (boards+doubles) + " (" + picName + ") - Fundet " + doubles + " dubletter, " + boards + " unikke");
+      System.out.println("Hentet plade " + (boards + doubles) + " (" + picName + ") - Fundet " + doubles + " dubletter, " + boards + " unikke");
+   }
+
+   private static void processImage(File rootDir, BufferedImage image, String kontrol) throws IOException {
+      Plade plade = ocrScanner.recognize(image);
+      plade.setKontrolKode(kontrol);
+      String fileTitle = plade.getFileTitle();
+
+      File targetDir = new File(rootDir, fileTitle);
+      if (!targetDir.exists()) {
+         targetDir.mkdirs();
+
+         File targetFile = new File(targetDir, fileTitle);
+         ImageIO.write(image, "PNG", targetFile);
+
+         writeTextData(plade, targetDir);
+
+         writeBinaryData(plade, targetDir);
+         boards++;
+      } else {
+         doubles++;
+      }
    }
 
    private static String loadFromUrl(String pageUrl) throws IOException {
       try {
          URL url = new URL(pageUrl);
          URLConnection urlConnection = url.openConnection();
-         urlConnection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; FDM)");
+         urlConnection.setRequestProperty("User-Agent", currentUserAgent);
          urlConnection.setRequestProperty("referer", "http://" + BINGOBANKO_URL + "/print");
          InputStream urlIs = urlConnection.getInputStream();
          BufferedReader rd = new BufferedReader(new InputStreamReader(urlIs));
@@ -180,10 +170,10 @@ public class BoardDownloader {
       }
    }
 
-   private BufferedImage readImage(String prefix, String picName) throws IOException {
+   private static BufferedImage readImage(String prefix, String picName) throws IOException {
       URL pictureURL = new URL("http://" + BINGOBANKO_URL + prefix + "/" + picName);
       URLConnection connection = pictureURL.openConnection();
-      connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; FDM)");
+      connection.setRequestProperty("User-Agent", currentUserAgent);
       connection.setRequestProperty("referer", "http://" + BINGOBANKO_URL + "/print");
       BufferedImage image;
       try {
@@ -197,7 +187,7 @@ public class BoardDownloader {
       return image;
    }
 
-   private void writeBinaryData(Plade plade, File targetDir) throws IOException {
+   private static void writeBinaryData(Plade plade, File targetDir) throws IOException {
       File targetOcrDat = new File(targetDir, "data.dat");
       FileOutputStream fileOutputStream = new FileOutputStream(targetOcrDat);
       ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
@@ -206,13 +196,13 @@ public class BoardDownloader {
       fileOutputStream.close();
    }
 
-   private void writeTextData(Plade plade, File targetDir) throws IOException {
+   private static void writeTextData(Plade plade, File targetDir) throws IOException {
       File targetOcr = new File(targetDir, "data.txt");
       FileWriter writer = new FileWriter(targetOcr, false);
 
       writer.append(plade.getKontrolKode() + "\r\n");
 
-      for(int i=0;i<plade.getLineCount();i++) {
+      for (int i = 0; i < plade.getLineCount(); i++) {
          ArrayList<Integer> list = plade.getLine(i);
          for (Integer integer : list) {
             writer.append(" " + integer);
@@ -220,5 +210,10 @@ public class BoardDownloader {
          writer.append("\r\n");
       }
       writer.close();
+   }
+
+   private static String pickUserAgent() {
+      double v = Math.random() * userAgents.length;
+      return userAgents[(int)v];
    }
 }
